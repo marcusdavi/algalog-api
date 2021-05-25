5ef95d8b09f7886364aa00a382c891e45422e2c7
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2021-05-24 22:01:53 -0300

    configuracao do Swagger

--- src/main/java/com/algaworks/algalog/AlgalogApiApplication.java
+++ src/main/java/com/algaworks/algalog/AlgalogApiApplication.java
@@ -1,13 +1,47 @@
 package com.algaworks.algalog;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AlgalogApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgalogApiApplication.class, args);
+	}
+	
+	@Bean
+	public Docket petApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algalog"))
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/")
+				.directModelSubstitute(LocalDate.class, String.class)
+				.genericModelSubstitutes(ResponseEntity.class)
+				.apiInfo(apiInfo());
+
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Entregas - REST API")
+				.version("1.0")
+				.build();
 	}
 
 }
